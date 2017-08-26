125a84803645ef28d75c47d7c32e3021a894a0e9
ShiNoSenshi <darksenshi@gmail.com>
2017-08-26 17:25:47 +0200

    Removed comments.

--- Assets/Scripts/AttackKeyFactory.cs
+++ Assets/Scripts/AttackKeyFactory.cs
@@ -3,9 +3,6 @@
 internal class AttackKeyFactory : ObjectFactory
 {
     private static AttackKeyFactory _instance;
-    /*private Sprite menSprite;
-    private Sprite koteSprite;
-    private Sprite doSprite;*/
 
     public static AttackKeyFactory instance
     {
@@ -19,47 +16,8 @@ internal class AttackKeyFactory : ObjectFactory
         }
     }
 
-    /*private void LoadSprites()
-    {
-        Texture2D menTex = (Texture2D)Resources.Load("KeyMen");
-        menSprite = Sprite.Create(menTex, new Rect(0.0f, 0.0f, menTex.width, menTex.height), new Vector2(0.5f, 0.5f), 100.0f);
-
-        Texture2D koteTex = (Texture2D)Resources.Load("KeyKote");
-        koteSprite = Sprite.Create(koteTex, new Rect(0.0f, 0.0f, koteTex.width, koteTex.height), new Vector2(0.5f, 0.5f), 100.0f);
-
-        Texture2D doTex = (Texture2D)Resources.Load("KeyDo");
-        doSprite = Sprite.Create(doTex, new Rect(0.0f, 0.0f, doTex.width, doTex.height), new Vector2(0.5f, 0.5f), 100.0f);
-    }*/
-
     private AttackKeyFactory()
     {
         LoadSprites("KeyMen", "KeyKote", "KeyDo");
     }
-
-    /*public GameObject GenerateKey(Attack key, Vector3 position)
-    {
-        Sprite sprite = null;
-        switch (key)
-        {
-            case Attack.Men:
-                sprite = menSprite;
-                break;
-            case Attack.Kote:
-                sprite = koteSprite;
-                break;
-            case Attack.Do:
-                sprite = doSprite;
-                break;
-            default:
-                return null;
-        }
-
-        var keyObject = new GameObject("KeyObject");
-        keyObject.transform.position = position;
-        keyObject.transform.localScale = new Vector3(0.2f, 0.2f);
-        keyObject.AddComponent<SpriteRenderer>();
-        var spriteRenderer = keyObject.GetComponent<SpriteRenderer>();
-        spriteRenderer.sprite = sprite;
-        return keyObject;
-    }*/
 }
--- Assets/Scripts/EnemyFactory.cs
+++ Assets/Scripts/EnemyFactory.cs
@@ -4,9 +4,6 @@ using UnityEditor;
 internal class EnemyFactory : ObjectFactory
 {
     private static EnemyFactory _instance;
-    /*private Sprite menSprite;
-    private Sprite koteSprite;
-    private Sprite doSprite;*/
 
     public static EnemyFactory instance
     {
@@ -20,47 +17,8 @@ internal class EnemyFactory : ObjectFactory
         }
     }
 
-    /*private void LoadSprites()
-    {
-        Texture2D menTex = (Texture2D)Resources.Load("CharacterMen");
-        menSprite = Sprite.Create(menTex, new Rect(0.0f, 0.0f, menTex.width, menTex.height), new Vector2(0.5f, 0.5f), 100.0f);
-
-        Texture2D koteTex = (Texture2D)Resources.Load("CharacterKote");
-        koteSprite = Sprite.Create(koteTex, new Rect(0.0f, 0.0f, koteTex.width, koteTex.height), new Vector2(0.5f, 0.5f), 100.0f);
-
-        Texture2D doTex = (Texture2D)Resources.Load("CharacterDo");
-        doSprite = Sprite.Create(doTex, new Rect(0.0f, 0.0f, doTex.width, doTex.height), new Vector2(0.5f, 0.5f), 100.0f);
-    }*/
-
     private EnemyFactory()
     {
         LoadSprites("CharacterMen", "CharacterKote", "CharacterDo");
     }
-
-    /*public GameObject GenerateEnemy(Attack key, Vector3 position)
-    {
-        Sprite sprite = null;
-        switch (key)
-        {
-            case Attack.Men:
-                sprite = menSprite;
-                break;
-            case Attack.Kote:
-                sprite = koteSprite;
-                break;
-            case Attack.Do:
-                sprite = doSprite;
-                break;
-            default:
-                return null;
-        }
-
-        var enemyObject = new GameObject("KeyObject");
-        enemyObject.transform.position = position;
-        enemyObject.transform.localScale = new Vector3(0.2f, 0.2f);
-        enemyObject.AddComponent<SpriteRenderer>();
-        var spriteRenderer = enemyObject.GetComponent<SpriteRenderer>();
-        spriteRenderer.sprite = sprite;
-        return enemyObject;
-    }*/
 }
